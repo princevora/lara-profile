490c4ac5fddc01d0ae09c25c4d3c2f17b7d863e3
Prince <princevora684@gmail.com>
2024-11-07 10:57:26 +0530

    update embed

--- resources/views/pages/api/embed/profile.blade.php
+++ resources/views/pages/api/embed/profile.blade.php
@@ -16,7 +16,7 @@
 <body class="relative min-h-screen flex flex-col bg-gray-100 text-gray-600 dark:text-gray-400 dark:bg-gray-900">
 
     <div class="flex-1 select-none flex flex-col justify-center items-center overflow-auto">
-        
+
         <x-render-user-profile :profile="$profile" :badges="$badges" />
 
         <!-- GitHub Profile Branding -->
@@ -32,7 +32,8 @@
                     class="border relative border-blue-200 inline-flex items-center p-0.5 pb-0.5 pl-2.5 rounded-full gap-x-2 border-white/40 dark:border-white/30 bg-gray-950/20 dark:bg-white/10 before:scale-y-110 before:absolute before:inset-x-4 before:-bottom-px before:bg-gradient-to-r before:from-transparent before:via-yellow-50 before:to-transparent before:h-px before:w-3/5">
                     <a href="https://github.com/princevora/lara-profile" target="_blank">
                         <span class="text-sm tracking-wide text-white">LARA - PROFILE &nbsp;</span>
-                        <span class="px-2 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gray-950/20 dark:bg-white/20 border border-white/30 rounded-full">
+                        <span
+                            class="px-2 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gray-950/20 dark:bg-white/20 border border-white/30 rounded-full">
                             @princevora
                         </span>
                     </a>
@@ -43,8 +44,11 @@
 
     <!-- Powered by Lara Profile (At the bottom) -->
     <div class="mt-auto text-center text-xs text-gray-500 dark:text-gray-400 py-2">
-        Powered by <a href="https://yourwebsite.com" class="text-blue-500 hover:underline">Lara Profile</a>
+        Powered by <a href="https://yourwebsite.com" class="text-blue-500 hover:underline">
+            Lara Profile
+        </a>
     </div>
+
 
     @stack('scripts')
 </body>
