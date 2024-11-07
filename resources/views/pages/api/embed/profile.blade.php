a9d4b2ae1ca6fa9c31a8ccbf706297d1d79d7b87
Prince <princevora684@gmail.com>
2024-11-07 10:58:01 +0530

    update embed

--- resources/views/pages/api/embed/profile.blade.php
+++ resources/views/pages/api/embed/profile.blade.php
@@ -44,7 +44,7 @@
 
     <!-- Powered by Lara Profile (At the bottom) -->
     <div class="mt-auto text-center text-xs text-gray-500 dark:text-gray-400 py-2">
-        Powered by <a href="https://yourwebsite.com" class="text-blue-500 hover:underline">
+        Powered by <a href="https://yourwebsite.com" class="text-blue-500 hover:underline dark:text-blue-400">
             Lara Profile
         </a>
     </div>
